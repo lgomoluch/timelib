188e14cf3762383d50e0e7bebc8a7916fcbc5a68
lgomoluch <lothar.gomoluch@web.de>
2020-02-25 10:40:27 +0100

    Refactoring

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ int main()
 
     input_date(&day, &month, &year);
 
-    printf("Tag des Jahres: %i\n", weekday_as_number(day, month, year));
+    weekday_as_number(day, month, year);
+    printf("Tag des Jahres: %i\n", day_of_the_year(day,month,year));
     return 0;
 }
